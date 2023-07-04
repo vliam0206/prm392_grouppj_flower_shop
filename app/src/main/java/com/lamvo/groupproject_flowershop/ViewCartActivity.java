72c3d4c03801f43b51354967495dc62b028b8207
danghoanganh36@gmail.com <danghoanganh36@gmail.com>
2023-07-04 17:03:50 +0700

    Anh - Create UI for Cart

---
+++ app/src/main/java/com/lamvo/groupproject_flowershop/ViewCartActivity.java
@@ -0,0 +1,14 @@
+package com.lamvo.groupproject_flowershop;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ViewCartActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cart);
+    }
+}
