bdc225cdeadc0619a5f58051aaa74a505015b1b2
trungthongnguyen2002@gmail.com <trungthongnguyen2002@gmail.com>
2023-07-16 22:54:11 +0700

    [Thong][Display marrkers on map]

---
+++ app/src/main/java/com/lamvo/groupproject_flowershop/ViewMapActivity.java
@@ -0,0 +1,14 @@
+package com.lamvo.groupproject_flowershop;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ViewMapActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_view_map);
+    }
+}
