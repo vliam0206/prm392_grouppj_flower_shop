17313774e647a4392855e42fa39c92ed21d75d50
danghoanganh36@gmail.com <danghoanganh36@gmail.com>
2023-07-19 20:11:11 +0700

    Anh - Add Menu Order

---
+++ app/src/main/java/com/lamvo/groupproject_flowershop/activities/OrderActivity.java
@@ -0,0 +1,16 @@
+package com.lamvo.groupproject_flowershop.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.lamvo.groupproject_flowershop.R;
+
+public class OrderActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_order);
+    }
+}
