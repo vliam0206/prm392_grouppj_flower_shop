617c7805676bfe17ba17c578c1ccf8c0c3b2be24
danghoanganh36@gmail.com <danghoanganh36@gmail.com>
2023-07-19 23:51:17 +0700

    Anh - Create Order List UI

--- app/src/main/java/com/lamvo/groupproject_flowershop/activities/OrderActivity.java
+++ app/src/main/java/com/lamvo/groupproject_flowershop/activities/OrderActivity.java
@@ -12,5 +12,7 @@ public class OrderActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_order);
+
+
     }
 }
